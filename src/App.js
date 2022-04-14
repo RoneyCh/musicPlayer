d7c7b1226a86058c75698bf6def0f6a8e6241709
RoneyCh <roneychristian159@gmail.com>
2022-04-13 21:52:21 -0300

    adding play pause

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
-import data from './data';
+import data from "./data";
 
 function App() {
   //State
   const [songs, setSongs] = useState(data);
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div>
-     <Song currentSong={currentSong}/>
-     <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
